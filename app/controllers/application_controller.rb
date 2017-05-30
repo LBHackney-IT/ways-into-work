0f89412ce213c50ffe096db927ecf9c11af467b6
Chris Evans <evanschris@me.com>
2017-05-30 13:11:16 +0100

    add in unique headers for each user type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user_login!
   before_action :register_new_client_to_login
 
+  helper_method :after_sign_in_path_for
+
   layout 'default'
 
   def after_sign_in_path_for(resource_or_scope)
