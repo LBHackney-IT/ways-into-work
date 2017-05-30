d7e0d83c552b8c7670f4dee1b05d82df1af3fa7d
Jason Rae <jrae@wearefuturegov.com>
2017-05-30 14:00:32 +0100

    Simplfied logic in header layout -> helper
    Also added another header for registration page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def current_header
+    if !user_login_signed_in?
+      'layouts/site_header_base'
+    elsif current_user_login.user_id.blank?
+      'layouts/site_header_client_registration'
+    elsif current_user_login.user_type == 'Client'
+      'layouts/site_header_service_manager'
+    else
+      'layouts/site_header_client'
+    end
+  end
 end
