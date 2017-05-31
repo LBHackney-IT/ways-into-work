31eafc8108e9526a9d88b8035c1b49e7627eaeb9
Chris Evans <evanschris@me.com>
2017-05-31 14:16:13 +0100

    on load sort checkboxes

--- app/assets/javascripts/form-helper.js
+++ app/assets/javascripts/form-helper.js
@@ -7,12 +7,22 @@ $.fn.handleFormElements = function() {
   convertCheckbox = $(this).find('.convert_checkbox')
 
   if(convertCheckbox.length)
+    convertCheckbox.find('input[type=checkbox]').each(function() {
+      if ($(this).is(':checked')) {
+        $(this).parents('.checkbox').addClass('is-primary is-outlined');
+      }
+    });
     convertCheckbox.find('input[type=checkbox]').click(function() {
       $(this).parents('.checkbox').toggleClass('is-primary is-outlined');
     });
 
   if(toggleOther.length)
     textFieldBlock = toggleOther.parents('#other').find('#other_text');
+    console.log(textFieldBlock.find('input[type=text]').val());
+    if (textFieldBlock.find('input[type=text]').val() != '') {
+      toggleOther.parents('.checkbox').addClass('is-primary is-outlined');
+      textFieldBlock.removeClass('is-hidden');
+    }
 
     toggleOther.change(function() {
       textFieldBlock.toggleClass('is-hidden');
