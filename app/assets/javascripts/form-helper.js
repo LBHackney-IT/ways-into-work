11def66efbc0f3dbb963d28dba34d2c44e50ea68
Chris Evans <evanschris@me.com>
2017-05-31 15:01:05 +0100

    change to styling on the label to ensure whole button is clickable

--- app/assets/javascripts/form-helper.js
+++ app/assets/javascripts/form-helper.js
@@ -9,18 +9,18 @@ $.fn.handleFormElements = function() {
   if(convertCheckbox.length)
     convertCheckbox.find('input[type=checkbox]').each(function() {
       if ($(this).is(':checked')) {
-        $(this).parents('.checkbox').addClass('is-primary is-outlined');
+        $(this).parents('.checkbox label').addClass('is-primary is-outlined');
       }
     });
     convertCheckbox.find('input[type=checkbox]').click(function() {
-      $(this).parents('.checkbox').toggleClass('is-primary is-outlined');
+      $(this).parents('.checkbox label').toggleClass('is-primary is-outlined');
     });
 
   if(toggleOther.length)
     textFieldBlock = toggleOther.parents('#other').find('#other_text');
     console.log(textFieldBlock.find('input[type=text]').val());
     if (textFieldBlock.find('input[type=text]').val() != '') {
-      toggleOther.parents('.checkbox').addClass('is-primary is-outlined');
+      toggleOther.parents('.checkbox label').addClass('is-primary is-outlined');
       textFieldBlock.removeClass('is-hidden');
     }
 
@@ -28,6 +28,8 @@ $.fn.handleFormElements = function() {
       textFieldBlock.toggleClass('is-hidden');
       if(textFieldBlock.hasClass('is-hidden')) {
         textFieldBlock.find('input[type="text"]').val('');
+      } else {
+        textFieldBlock.find('input[type="text"]').focus();
       }
     });
 
