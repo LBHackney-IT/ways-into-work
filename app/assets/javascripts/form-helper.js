d42c5cdbf8a45566c5be9103a079463e8b4480af
Chris Evans <evanschris@me.com>
2017-05-31 13:09:09 +0100

    add in checkbox conversion for three personal pages

--- app/assets/javascripts/form-helper.js
+++ app/assets/javascripts/form-helper.js
@@ -4,8 +4,15 @@ $.fn.handleFormElements = function() {
 
   toggleOther = $(this).find('#toggle_other')
 
+  convertCheckbox = $(this).find('.convert_checkbox')
+
+  if(convertCheckbox.length)
+    convertCheckbox.find('input[type=checkbox]').click(function() {
+      $(this).parents('.checkbox').toggleClass('is-primary is-outlined');
+    });
+
   if(toggleOther.length)
-    textFieldBlock = toggleOther.siblings('#other_text')
+    textFieldBlock = toggleOther.parents('#other').find('#other_text');
 
     toggleOther.change(function() {
       textFieldBlock.toggleClass('is-hidden');
