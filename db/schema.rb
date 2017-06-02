14953d77223d20f3d26ac0025242182911f2f01a
Jason Rae <jrae@wearefuturegov.com>
2017-06-02 18:31:25 +0100

    Making the education selection an enum
    Otherwise it would end up being two booleans

--- db/schema.rb
+++ db/schema.rb
@@ -36,12 +36,12 @@ ActiveRecord::Schema.define(version: 20170602182708) do
     t.string "objectives", default: [], array: true
     t.string "other_objective"
     t.boolean "employed"
-    t.boolean "studying"
     t.integer "working_hours_per_week"
     t.string "time_since_last_job"
     t.string "job_title"
     t.string "current_education"
     t.string "past_education"
+    t.integer "studying", default: 0
   end
 
   create_table "service_managers", force: :cascade do |t|
