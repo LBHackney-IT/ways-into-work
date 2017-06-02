725c1e52cdce791aa142344be97b75b24be7d145
Jason Rae <jrae@wearefuturegov.com>
2017-06-02 15:52:58 +0100

    Adding new view to create password on confirming email

---
+++ app/controllers/client/passwords_controller.rb
@@ -0,0 +1,20 @@
+class Client::PasswordsController < ApplicationController
+  skip_before_action :authenticate_user_login!
+  before_action :assert_reset_token_passed, only: :edit
+
+  def edit
+    @user_login = UserLogin.find(params[:user_login_id])
+    @minimum_password_length = UserLogin.password_length.min
+    @user_login.reset_password_token = params[:reset_password_token]
+  end
+
+
+    # Check if a reset_password_token is provided in the request
+  def assert_reset_token_passed
+    if params[:reset_password_token].blank?
+      set_flash_message(:alert, :no_token)
+      redirect_to new_user_login_session_path
+    end
+  end
+
+end
