82717da980f29888d8287f4d85d22400177a362a
Jason Rae <jrae@wearefuturegov.com>
2017-06-21 15:13:29 +0100

    Remove validation from advisor assignment

--- app/controllers/service_manager/client_advisors_controller.rb
+++ app/controllers/service_manager/client_advisors_controller.rb
@@ -1,12 +1,12 @@
 class ServiceManager::ClientAdvisorsController < ServiceManager::BaseController
 
   def update
-    @client = Client.find(params[:client_id])
-    if @client.update_attributes(client_params)
+    client = Client.find(params[:client_id])
+    client.advisor = Advisor.find(client_params[:advisor_id])
+    if client.advisor && client.save(validate: false)
       flash[:success] = I18n.t('clients.flashes.success.advisor_assigned')
     else
-      # TODO fugly - should really render errors in the form
-      flash[:error] = I18n.t('clients.flashes.error.advisor_assignment', errors: @client.errors.messages)
+      flash[:error] = I18n.t('clients.flashes.error.advisor_assignment')
     end
     redirect_to service_manager_client_path(id: params[:client_id])
   end
