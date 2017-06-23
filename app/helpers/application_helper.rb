56b63f634d683b96cc54d529ce147f39458c8d76
Jason Rae <jrae@wearefuturegov.com>
2017-06-23 11:55:22 +0100

    A little refactoring to add an Advisor Header
    Remove duplicate alerts in client header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,8 @@ module ApplicationHelper
       'layouts/site_header_client_registration'
     elsif current_user_login.user_type == 'Client'
       'layouts/site_header_client'
+    elsif current_user_login.user_type == 'Advisor'
+      'layouts/site_header_advisor'
     else
       'layouts/site_header_service_manager'
     end
