5a5868cfc7184f34d2c9afbfd779fecdf337bcbb
Chris Evans <evanschris@users.noreply.github.com>
2017-06-27 16:09:08 +0100

    Update to advisor table styling

--- app/models/client.rb
+++ app/models/client.rb
@@ -26,6 +26,11 @@ class Client < ApplicationRecord
    "#{first_name} #{last_name}"
   end
 
+  def age
+    now = Time.now.utc.to_date
+    now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
+  end
+
   def valid_postcode?
     return false if self.postcode.blank?
     if better_postcode = GoingPostal.postcode?(postcode, 'GB')
