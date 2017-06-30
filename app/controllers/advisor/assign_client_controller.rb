15d6208ea39b2c66534a5c4379764a838e827c36
Jason Rae <jrae@wearefuturegov.com>
2017-06-30 12:08:44 +0100

    Adding email notification on assignment of client

--- app/controllers/advisor/assign_client_controller.rb
+++ app/controllers/advisor/assign_client_controller.rb
@@ -4,6 +4,7 @@ class Advisor::AssignClientController < Advisor::BaseController
     client = Client.find(params[:client_id])
     client.advisor = Advisor.find(client_params[:advisor_id])
     if client.advisor && client.save(validate: false)
+      AdvisorMailer.notify_assigned(client).deliver_now unless current_advisor == client.advisor
       flash[:success] = I18n.t('clients.flashes.success.advisor_assigned')
     else
       flash[:error] = I18n.t('clients.flashes.error.advisor_assignment')
--- lib/mail_previews/service_manager_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-class ServiceManagerMailerPreview < ActionMailer::Preview
-
-  def notify_client_signed_up
-    ServiceManagerMailer.notify_client_signed_up(Fabricate.build(:client))
-  end
-end
