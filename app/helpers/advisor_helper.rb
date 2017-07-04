1c0dc883fdc37dbf549e464079fb1387ef6c2ae5
Chris Evans <evanschris@users.noreply.github.com>
2017-07-04 14:24:21 +0100

    Update table columns

--- app/helpers/advisor_helper.rb
+++ app/helpers/advisor_helper.rb
@@ -1,9 +1,10 @@
 module AdvisorHelper
   def number_of_days_waiting(date)
     if(date != Date.today)
-      return " - waiting #{pluralize((Date.today - date).to_i, 'day')}"
+      " - waiting #{pluralize((Date.today - date).to_i, 'day')}"
     else
-      return " - today"
+      " - today"
     end
   end
+
 end
