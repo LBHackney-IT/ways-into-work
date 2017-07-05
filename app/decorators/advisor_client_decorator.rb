7bad8b41aeebe29b3be28d7aef07c3159fa9ad7b
Jason Rae <jrae@wearefuturegov.com>
2017-07-05 12:08:36 +0100

    Temp add schedule another meeting to details page
    After filling in an assessment Advisors need to be able to schedule another
    appointment
    
    This logic is a mess and styled badly. Just propping up for show and tell
    until we have feedback on what they need to be able to do

--- app/decorators/advisor_client_decorator.rb
+++ app/decorators/advisor_client_decorator.rb
@@ -6,11 +6,12 @@ class AdvisorClientDecorator < ClientDecorator
 
   def decorate_file_action
     if latest_cv.present?
-      h.link_to(latest_cv.attachment_file_name, latest_cv.attachment.try(:url), title: latest_cv.attachment_file_name, target: :blank) <<
-      h.content_tag(:p, "CV uploaded by: #{latest_cv.uploaded_by} on #{latest_cv.created_at.to_date.to_s(:short)}") <<
-      h.button_to(I18n.t('clients.buttons.replace_cv'), h.advisor_client_file_upload_path(client, latest_cv), method: :delete, class: 'button is-primary')
+      h.content_tag(:span, "CV: ") <<
+      h.link_to("#{latest_cv.attachment_file_name}", latest_cv.attachment.try(:url), title: latest_cv.attachment_file_name, target: :blank) <<
+      h.content_tag(:p, "Uploaded by: #{latest_cv.uploaded_by} on #{latest_cv.created_at.to_date.to_s(:short)}") <<
+      h.button_to(I18n.t('clients.buttons.replace_cv'), h.advisor_client_file_upload_path(client, latest_cv), method: :delete, class: 'button is-primary is-small')
     else
-      h.link_to I18n.t('clients.buttons.upload_cv'), h.new_advisor_client_file_upload_path(client), class: "button is-primary"
+      h.link_to I18n.t('clients.buttons.upload_cv'), h.new_advisor_client_file_upload_path(client), class: "button is-primary is-small"
     end
   end
 
