95b9ebcb6fe6e17abe67da4dce536551235add4c
Jason Rae <jrae@wearefuturegov.com>
2017-07-12 00:09:01 +0100

    Hooking up meetings for edit and update
    Boiler plate code. Needs to be moved from the actions partial

--- app/controllers/advisor/meetings_controller.rb
+++ app/controllers/advisor/meetings_controller.rb
@@ -1,16 +1,36 @@
 class Advisor::MeetingsController < Advisor::BaseController
 
+  def index
+    @client = Client.find_by(id: params[:client_id])
+  end
+
   def new
     @client = Client.find_by(id: params[:client_id])
     @meeting = @client.meetings.build(advisor_id: current_advisor.id)
     @meeting.agenda = 'initial_assessment' if @client.meetings.count == 0
   end
 
+  def edit
+    @client = Client.find_by(id: params[:client_id])
+    @meeting = @client.meetings.find(params[:id])
+  end
+
 
   def create
     @client = current_advisor.clients.find_by(id: params[:client_id])
     if @meeting = @client.meetings.create(meeting_params)
+      flash[:success] = "Meeting saved"
       redirect_to :advisor_my_clients
+    else
+      render :new
+    end
+  end
+
+  def update
+    @meeting = Meeting.find(params[:id])
+    if @meeting.update_attributes(meeting_params)
+      flash[:success] = "Meeting updated"
+      redirect_to advisor_client_meetings_path(client_id: @meeting.client_id)
     else
       render :new
     end
--- app/decorators/advisor_client_decorator.rb
+++ app/decorators/advisor_client_decorator.rb
@@ -7,4 +7,12 @@ class AdvisorClientDecorator < ClientDecorator
   def new_file_button
     h.link_to I18n.t('clients.buttons.upload_cv'), h.new_advisor_client_file_upload_path(client), class: "button is-primary is-small"
   end
+
+  def decorate_meetings_action
+    if client.meetings.any?
+      h.link_to I18n.t('clients.buttons.view_meetings'), h.advisor_client_meetings_path(client), method: :get, class: "button is-primary is-small"
+    else
+      h.button_to I18n.t('clients.buttons.arrange_meeting'), h.new_advisor_client_meeting_path(client), method: :get, class: "button is-primary is-small"
+    end
+  end
 end
