ee6b46a177e9982961a612b1daae9c36919e9d8f
Jason Rae <jrae@wearefuturegov.com>
2017-07-13 00:36:12 +0100

    More refactoring with decent exposure
    Delete button needs to be restyled to be outside the
    update form

--- app/controllers/advisor/action_plan_tasks_controller.rb
+++ app/controllers/advisor/action_plan_tasks_controller.rb
@@ -1,24 +1,21 @@
 class Advisor::ActionPlanTasksController < Advisor::BaseController
 
+  expose :client
+  expose :new_action_plan_task, -> { client.action_plan_tasks.build }
+  expose :action_plan_task
+
   def index
-    @client = Client.find(params[:client_id])
   end
 
   def new
-    @client = Client.find(params[:client_id])
-    @action_plan = @client.action_plan_tasks.build
   end
 
   def edit
-    @client = Client.find(params[:client_id])
-    @action_plan_task = ActionPlanTask.find(params[:id])
   end
 
   def create
-    @client = Client.find(params[:client_id])
-    @action_plan = @client.action_plan_tasks.build(task_params)
-    if @action_plan.save
-      flash[:success] = "Agreed Action: #{@action_plan.title} saved"
+    if action_plan_task.save
+      flash[:success] = "Agreed Action: #{action_plan_task.title} saved"
       redirect_to redirect_path_from_commit
     else
       render :new
@@ -26,19 +23,17 @@ class Advisor::ActionPlanTasksController < Advisor::BaseController
   end
 
   def update
-    @action_plan_task = ActionPlanTask.find(params[:id])
-    if @action_plan_task.update_attributes(task_params)
-      flash[:success] = "Agreed Action: #{@action_plan_task.title} saved"
-      redirect_to advisor_client_action_plan_tasks_path(client_id: @action_plan_task.client_id)
+    if action_plan_task.update(action_plan_task_params)
+      flash[:success] = "Agreed Action: #{action_plan_task.title} saved"
+      redirect_to advisor_client_action_plan_tasks_path(client_id: action_plan_task.client_id)
     else
       render :new
     end
   end
 
   def destroy
-    @action_plan_task = ActionPlanTask.find(params[:id])
-    flash[:success] = "Agreed Action: #{@action_plan_task.title} deleted"
-    @action_plan_task.destroy!
+    flash[:success] = "Agreed Action: #{action_plan_task.title} deleted"
+    action_plan_task.destroy!
     redirect_to advisor_client_action_plan_tasks_path(client_id: params[:client_id])
   end
 
@@ -47,17 +42,18 @@ class Advisor::ActionPlanTasksController < Advisor::BaseController
 
   def redirect_path_from_commit
     if params[:commit] == 'Save'
-      advisor_client_action_plan_tasks_path(client_id: @client.id)
+      advisor_client_action_plan_tasks_path(client_id: client.id)
     else
-      new_advisor_client_action_plan_tasks_path(client_id: @client.id)
+      new_advisor_client_action_plan_tasks_path(client_id: client.id)
     end
   end
 
-  def task_params
+  def action_plan_task_params
     params.require(:action_plan_task).permit([
       :title,
       :notes,
       :advisor_id,
+      :client_id,
       :due_date,
       :status
       ])
