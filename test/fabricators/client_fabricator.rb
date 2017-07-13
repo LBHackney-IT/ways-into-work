7a37e8af611ea5ccd1c5881ffaa6187c5a51b67a
Jason Rae <jrae@wearefuturegov.com>
2017-07-14 00:23:17 +0100

    Add age filter to my clients

--- test/fabricators/client_fabricator.rb
+++ test/fabricators/client_fabricator.rb
@@ -4,7 +4,7 @@ Fabricator(:client) do
   phone '07000 123456'
   address_line_1  'London'
   postcode 'E8 1EA'
-  date_of_birth { Time.now - 30.years }
+  date_of_birth { Time.now - rand(18..35).years }
   login { Fabricate.build(:user_login) }
   advisor { Fabricate(:team_leader) }
 end
