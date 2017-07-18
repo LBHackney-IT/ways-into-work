97d9aebeeb65fb90cd30bbe14c0d4949c2831374
Jason Rae <jrae@wearefuturegov.com>
2017-07-18 11:58:39 +0100

    Fixed issue with the way decent exposure was used

--- app/controllers/advisor/contact_notes_controller.rb
+++ app/controllers/advisor/contact_notes_controller.rb
@@ -1,17 +1,18 @@
 class Advisor::ContactNotesController < Advisor::BaseController
 
   expose :client, decorate: ->(client){ client.decorate }
+
   expose :contact_note
-  expose :new_contact_note, -> { client.contact_notes.build(advisor_id: current_advisor.id, contact_method: client.preferred_contact_method) }
 
   def new
+    init_contact_note
   end
 
   def create
     if contact_note.save
-      flash[:success] = "Contact Nor saved saved"
       redirect_to :advisor_my_clients
     else
+      flash[:success] = "Failed to save contact"
       render :new
     end
   end
@@ -24,6 +25,12 @@ class Advisor::ContactNotesController < Advisor::BaseController
       :client_id,
       :content,
       :contact_method)
+  end
+
+  def init_contact_note
+    contact_note.advisor_id = current_advisor.id
+    contact_note.client_id = params[:client_id]
+    contact_note.contact_method = client.preferred_contact_method
   end
 
 end
