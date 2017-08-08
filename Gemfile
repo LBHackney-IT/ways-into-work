f17baaaa62a139027ed766dacf68d6675d328564
Jason Rae <jrae@wearefuturegov.com>
2017-08-08 18:20:42 +0100

    Updating production seeding

--- Gemfile
+++ Gemfile
@@ -33,15 +33,16 @@ gem 'filterrific'
 gem 'kaminari'
 gem 'pg_search'
 
-# TODO - move to staging group (not needed in production)
-gem 'fabrication'
-gem 'ffaker'
-
 gem 'appsignal'
 
 gem 'decent_exposure'
 
 gem 'paranoia'
+
+group :development, :staging, :test do
+  gem 'fabrication'
+  gem 'ffaker'
+end
 
 group :development do
   gem 'bullet'
--- lib/seeders/client_seeder.rb
+++ lib/seeders/client_seeder.rb
@@ -14,9 +14,11 @@ class ClientSeeder
   end
 
   def import
+    @failures = ''
     @csv_data.each do |row|
       import_client(row)
     end
+    puts "@failures #{@failures}"
   end
 
   def is_number?(obj)
@@ -30,28 +32,30 @@ class ClientSeeder
     puts "importing #{client.name}"
     puts "updating existing record" if client.id
 
-    if row[:email]
+    if row['Email Address'].blank?
+      @failures << "Not importing #{row['First Name']} #{row['Surname']} without an Email Address\n"
+    elsif row['Contact Number'].blank?
+      @failures << "Not importing #{row['First Name']} #{row['Surname']} without a Phone Number\n"
+    else
+      puts "email #{row['Email Address']}"
       begin
         client.tap do |c|
           c.advisor_id = row["Advisor"]
           c.phone = row['Contact Number']
           c.postcode = row['Postcode'] || 'N4 2HF'
-          c.login = UserLogin.new(email: row['Email Address']) if (row['Email Address'] && c.login.blank?)
-          c.funded = row["Claim stream (ESF, FSF, TF, LBH, Other)"]
+          c.login = UserLogin.new(email: row['Email Address'], password: Devise.friendly_token.first(20)) if c.login.blank?
+          c.funded = row["Claim stream (ESF, FSF, TF, LBH, Other)"] != 'LBH'
           c.rag_status = row['Current Status (RAGG)'].try(:downcase) || :un_assessed
-          c.assessment_notes = c.assessment_notes.reject{ |goal| goal.content_key == 'job_goal_1' || goal.content_key == 'job_goal_2' }
-          c.assessment_notes << generate_job_goals(row['Job Goals'])
+          c.assessment_notes = generate_job_goals(row['Job Goals']) if c.assessment_notes.empty?
         end
 
         # not all have email addresses
         client.save!
 
       rescue ActiveRecord::RecordInvalid => e
-        puts "\nerror thrown importing #{client.name} #{e}\n"
-        client.save(validate: false)
+        @failures << "error thrown importing #{client.name} #{e}\n"
+        # client.save(validate: false)
       end
-    else
-      puts "Not importing #{row['First Name']} #{row['Surname']} without an email address"
     end
   end
 
