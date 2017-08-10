53e3e12699419378281a6a2300068a0959c03469
Jason Rae <jrae@wearefuturegov.com>
2017-08-10 11:07:39 +0100

    Fixed seeding issue for production

--- db/fixtures/production/01_advisors.rb
+++ db/fixtures/production/01_advisors.rb
@@ -5,7 +5,7 @@ advisors = [
     phone: "020 8356 5934",
     hub_id: 2
   }, {
-    email: "Richard.allen@hackney.gov.uk",
+    email: "richard.allen@hackney.gov.uk",
     name: 'Richard Alle',
     phone: "020 8356 2497",
     hub_id: 2
@@ -15,7 +15,7 @@ advisors = [
     phone: "0795 633 5906",
     hub_id: 2
   },{
-    email: "Caroline.modest@hackney.gov.uk",
+    email: "caroline.modest@hackney.gov.uk",
     name: "Caroline Modest",
     team_leader: true,
     phone: "020 8356 8189",
