9dd7dd192c6ab6342e47b67a9cfca918fcd4199a
Chris Evans <evanschris@users.noreply.github.com>
2017-08-10 14:16:06 +0100

    Fix broken columns in my clients page
    Also updated downtown copy to include number of hidden clients

--- app/assets/javascripts/clients-needing-appointment.js
+++ app/assets/javascripts/clients-needing-appointment.js
@@ -5,12 +5,12 @@ $.fn.clients_needing_appointment = function() {
   $("#clients_needing_appointment #view_more").click(function() {
     if ($("#clients_needing_appointment").hasClass('open')) {
       console.log('test1')
-      $("#view_more span.line").html("See more clients")
+      $("#view_more span.line #swap-word").html("See")
       $("#clients_needing_appointment #hidden-section").slideUp();
     } else {
       $("#clients_needing_appointment #hidden-section").slideDown();
       console.log('test2')
-      $("#view_more span.line").html("See fewer clients")
+      $("#view_more span.line #swap-word").html("Hide")
     }
 
     $("#clients_needing_appointment").toggleClass('open');
