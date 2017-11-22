e34f91eac8b75f505447ba822fb6b480b54ba5fb
Jason Rae <jrae@wearefuturegov.com>
2017-11-22 13:13:19 +0000

    Fixed localhost type of development default mailer options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ WaysIntoWork::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_deliveries = false
 
 end
