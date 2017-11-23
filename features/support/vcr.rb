5bb96bec5a321dc8321d989f3cb601bcba3acd89
pezholio <pezholio@gmail.com>
2017-11-23 10:12:51 +0000

    Filter out AWS and other creds from VCR cassettes

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -3,7 +3,11 @@ require 'vcr'
 VCR.configure do |c|
   c.hook_into :webmock
   c.ignore_localhost = true
-  c.cassette_library_dir     = 'features/support/cassettes'
+  c.cassette_library_dir = 'features/support/cassettes'
+  # Filter out anything in the .env file
+  Dotenv::Environment.new(File.join Rails.root, '.env').each do |key, value|
+    c.filter_sensitive_data("<#{key}>") { value }
+  end
 end
 
 VCR.cucumber_tags do |t|
