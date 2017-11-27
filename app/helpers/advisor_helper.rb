4996a351f75f073bc1a333b400b875cc3979467f
pezholio <pezholio@gmail.com>
2017-11-27 16:38:23 +0000

    More fixes after pulling in from master

--- app/helpers/advisor_helper.rb
+++ app/helpers/advisor_helper.rb
@@ -1,11 +1,9 @@
 module AdvisorHelper
-
   def number_of_days_waiting(date)
-    if(date != Date.today)
-      " - waiting #{pluralize((Date.today - date).to_i, 'day')}"
+    if date != Time.zone.today
+      " - waiting #{pluralize((Time.zone.today - date).to_i, 'day')}"
     else
-      " - today"
+      ' - today'
     end
   end
-
 end
