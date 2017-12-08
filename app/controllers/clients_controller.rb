1e4e2f83eda3cbc6d39af28e2aa701506dcce12d
Stuart Harrison <pezholio@gmail.com>
2017-12-08 10:07:03 +0000

    Small refactor to postcode handling (#53)

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,30 +1,26 @@
 class ClientsController < ApplicationController
   expose :client
 
-  before_action :init_client, only: :create
-
   def new
     client.build_login
   end
 
   def create
-    if client.save
-      client.login.send_reset_password_instructions
+    if client.assign_area(client_params[:postcode]) == false
+      redirect_to(:outside_hackney) && return
+    elsif client.save
+      setup_client(client)
       redirect_to just_registered_path
-      AdvisorMailer.notify_client_signed_up(client).deliver_now
     else
       render :new
     end
   end
 
   private
-
-  def init_client
-    if client_params[:postcode].blank?
-      client.errors.add(:postcode, "can't be blank")
-    else
-      redirect_to :outside_hackney unless client.assign_area(client_params[:postcode])
-    end
+  
+  def setup_client(client)
+    client.login.send_reset_password_instructions
+    AdvisorMailer.notify_client_signed_up(client).deliver_now
   end
 
   def client_params # rubocop:disable Metrics/MethodLength
