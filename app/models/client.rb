7f4fe954b989b58e31827c2b98793fe818085939
Martin J Thompson <hiremyskills@martinjthompson.com>
2017-12-20 14:45:16 +0000

    Add new tabs, reorder current tabs, add temp attr_readers to model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,8 @@
 class Client < ApplicationRecord # rubocop:disable ClassLength
   include PgSearch
+  attr_reader :national_insurance_number
+  attr_reader :affected_by_benefit_cap
+  attr_reader :assigned_supported_employment
 
   acts_as_paranoid
 
