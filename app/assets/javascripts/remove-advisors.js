e06335ff4709f747e3197f97eb0ffadde492641b
pezholio <pezholio@gmail.com>
2017-12-21 09:33:50 +0000

    Hide advisors on load too

--- app/assets/javascripts/remove-advisors.js
+++ app/assets/javascripts/remove-advisors.js
@@ -2,11 +2,17 @@ $.fn.removeAdvisors = function() {
 
   if (this.length === 0) { return false; }
   
-  $(this).change(function() {
+  var id = $(this).val();
+  var hideAdvisors = function(id) {
     $('#advisor option').removeClass('is-hidden');
-    var id = $(this).val()
     if (id == "") { return false; }
     $('#advisor option[data-hub-id][data-hub-id!='+ id +']').addClass('is-hidden');
+  }
+  
+  hideAdvisors(id);
+  
+  $(this).change(function() {
+    hideAdvisors($(this).val());
   });
 
 };
