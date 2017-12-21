2ecc9cb6de014ab11c51e0c8048e879d5282d307
pezholio <pezholio@gmail.com>
2017-12-21 16:17:25 +0000

    Make organisation a select field

--- test/fabricators/referrer_fabricator.rb
+++ test/fabricators/referrer_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:referrer) do
   name { FFaker::Name.name }
-  organisation { FFaker::Company.name }
+  organisation { OrganisationOption.all[0].id }
   phone '07000 123456'
   email { FFaker::Internet.email }
   reason { FFaker::Lorem.paragraph }
