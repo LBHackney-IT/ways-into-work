7d7bc264817054462a9722a3b0929d1626bb3e7a
pezholio <pezholio@gmail.com>
2018-01-12 15:49:21 +0000

    Add scopes

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -20,4 +20,21 @@ RSpec.describe Achievement, type: :model do
     expect(subject.date_acheived).to eq(date)
   end
   
+  describe '#scopes' do
+    
+    it 'gets achievements by name' do
+      Fabricate.times(5, :achievement)
+      expect(Achievement.with_name('placement_volunteering')).to match_array([subject])
+    end
+    
+    it 'gets achievements awarded in a time period' do
+      achievements = Fabricate.times(5, :achievement) do
+        date_acheived(((Time.zone.today - 5.months)..(Time.zone.today - 4.months)).to_a.sample(1).first)
+      end
+      
+      expect(Achievement.acheived_in_period(Time.zone.today - 5.months, Time.zone.today - 4.months)).to match_array(achievements)
+    end
+    
+  end
+  
 end
