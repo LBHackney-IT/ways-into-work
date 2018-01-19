0f52c31904552088a20c1effa6b215e379b2ec7f
pezholio <pezholio@gmail.com>
2018-01-19 12:27:18 +0000

    Remove outcome from DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111165522) do
+ActiveRecord::Schema.define(version: 20180119122521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20180111165522) do
     t.bigint "advisor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "outcome"
     t.index ["advisor_id"], name: "index_action_plan_tasks_on_advisor_id"
     t.index ["client_id"], name: "index_action_plan_tasks_on_client_id"
   end
