a1154f896e3a792f9e3accbf558043160f9f4939
Jason Rae <jrae@wearefuturegov.com>
2018-01-24 11:05:32 -0800

    Remove redundant confirmation email
    This functionality has been stripped out in previous
    commits.

--- lib/mail_previews/devise_mailer_preview.rb
+++ lib/mail_previews/devise_mailer_preview.rb
@@ -1,8 +1,5 @@
 # :nocov:
 class DeviseMailerPreview < ActionMailer::Preview
-  def confirmation_instructions
-    Devise::Mailer.confirmation_instructions((Client.last || Fabricate.build(:client)).login, 'AToken')
-  end
 
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions((Client.last || Fabricate.build(:client)).login, 'test')
