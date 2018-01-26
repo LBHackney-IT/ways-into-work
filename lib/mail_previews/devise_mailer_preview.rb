06c92c1c70dd18d63fcfa5e5e1b779f326589b9b
pezholio <pezholio@gmail.com>
2018-01-26 11:52:02 +0000

    Correct Rubocop complaint

--- lib/mail_previews/devise_mailer_preview.rb
+++ lib/mail_previews/devise_mailer_preview.rb
@@ -1,6 +1,5 @@
 # :nocov:
 class DeviseMailerPreview < ActionMailer::Preview
-
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions((Client.last || Fabricate.build(:client)).login, 'test')
   end
