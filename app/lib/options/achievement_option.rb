35b5ffba2c96d9575a3b87ded1870757e3507810
Jason Rae <jrae@wearefuturegov.com>
2018-01-29 09:27:05 -0800

    Unified logic of icon to Achievement Option
    Simplified helper to only first_or_subsequent method
    Renamed manual to achieved. Not sure what ‘manual’ represented?

--- app/lib/options/achievement_option.rb
+++ app/lib/options/achievement_option.rb
@@ -1,16 +1,23 @@
 class AchievementOption < Option
+  attr_reader :icon
+
+  def initialize(id, name, icon)
+    super(id, name)
+    @icon = icon
+  end
+
   def self.all # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     [
-      new('cv_completed', I18n.t('advisors.achievement.cv_completed.manual')),
-      new('job_application', I18n.t('advisors.achievement.job_application.manual')),
-      new('interview', I18n.t('advisors.achievement.interview.manual')),
-      new('placement_volunteering', I18n.t('advisors.achievement.placement_volunteering.manual')),
-      new('training_started', I18n.t('advisors.achievement.training_started.manual')),
-      new('course_completed', I18n.t('advisors.achievement.course_completed.manual')),
-      new('job_start', I18n.t('advisors.achievement.job_start.manual')),
-      new('job_sustained', I18n.t('advisors.achievement.job_sustained.manual')),
-      new('boc_completed', I18n.t('advisors.achievement.boc_completed.manual')),
-      new('13_week_sustainment', I18n.t('advisors.achievement.13_week_sustainment.manual'))
+      new('cv_completed', I18n.t('advisors.achievement.cv_completed.achieved'), 'file-pdf-o'),
+      new('job_application', I18n.t('advisors.achievement.job_application.achieved'), 'paper-plane'),
+      new('interview', I18n.t('advisors.achievement.interview.achieved'), 'street-view'),
+      new('placement_volunteering', I18n.t('advisors.achievement.placement_volunteering.achieved'), 'handshake-o'),
+      new('training_started', I18n.t('advisors.achievement.training_started.achieved'), 'graduation-cap'),
+      new('course_completed', I18n.t('advisors.achievement.course_completed.achieved'), 'graduation-cap'),
+      new('job_start', I18n.t('advisors.achievement.job_start.achieved'), 'id-badge'),
+      new('job_sustained', I18n.t('advisors.achievement.job_sustained.achieved'), 'shield'),
+      new('boc_completed', I18n.t('advisors.achievement.boc_completed.achieved'), 'money'),
+      new('13_week_sustainment', I18n.t('advisors.achievement.13_week_sustainment.achieved'), 'shield')
     ]
   end
 end
