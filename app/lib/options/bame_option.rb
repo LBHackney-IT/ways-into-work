b4e9fb3d2fba0f19ba5300fe1b90002d7325da46
Jason Rae <jrae@wearefuturegov.com>
2018-01-30 15:02:40 -0800

    Add prefer not to say to all Equality options

--- app/lib/options/bame_option.rb
+++ app/lib/options/bame_option.rb
@@ -5,7 +5,8 @@ class BameOption < Option
       new('black', 'Black British / African / Caribbean'),
       new('mixed', 'Mixed'),
       new('white_british', 'White British'),
-      new('white', 'White Other')
+      new('white', 'White Other'),
+      new('private', 'Prefer not to say')
     ]
   end
 end
