74743c552a2a3cb7e67c9e63115ab8115372e573
Jason Rae <jrae@wearefuturegov.com>
2018-02-05 15:22:18 -0800

    Add new scopes for selecting meetings two hours and two days future

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -3,11 +3,23 @@ class Meeting < ApplicationRecord
 
   belongs_to :client, counter_cache: true
   belongs_to :advisor
-  
+
   before_save :add_upcoming_meeting_to_client
-  
+
+  scope :two_hours_from_now, lambda {
+    from = (Time.zone.now + 2.hours).beginning_of_hour
+    within(from, from + 1.hour)
+  }
+
+  scope :two_days_from_now, lambda {
+    from = (Time.zone.now + 2.days).beginning_of_day
+    within(from, from + 1.day)
+  }
+
+  scope :within, -> (from, to) { where('start_datetime > ? AND start_datetime < ?', from, to) }
+
   private
-  
+
   def add_upcoming_meeting_to_client
     return if client.upcoming_meetings.first&.start_datetime && start_datetime < client.upcoming_meetings.first&.start_datetime
     client.update_attribute(:next_meeting_date, start_datetime) # rubocop:disable Rails/SkipsModelValidations
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,21 +1,43 @@
 require 'spec_helper'
 
 RSpec.describe Meeting, type: :model do
-  
+
   let!(:client) { Fabricate(:client) }
   let!(:meeting) { Fabricate(:meeting, client: client, start_datetime: Time.zone.now + 3.days) }
-  
+
   describe '#add_next_meeting_date_to_client' do
-    
+
+
     it 'adds a next meeting date to a client' do
       expect(client.next_meeting_date).to eq(meeting.start_datetime)
     end
-    
+
     it 'leaves the next meeting date alone' do
       Fabricate(:meeting, start_datetime: Time.zone.now + 1.day, client: client)
       expect(client.next_meeting_date).to eq(meeting.start_datetime)
     end
-    
+
   end
-  
+
+  describe 'future scopes' do
+    let!(:meeting_in_two_hours) {
+      Fabricate(:meeting, client: client, start_datetime: Time.zone.now + 2.hours)
+    }
+    let!(:meeting_in_two_days) {
+      Fabricate(:meeting, client: client, start_datetime: Time.zone.now + 2.days)
+    }
+
+    it 'finds a meeting two hours from now' do
+      expect(Meeting.two_hours_from_now).to include(meeting_in_two_hours)
+      expect(Meeting.two_hours_from_now).not_to include(meeting)
+      expect(Meeting.two_hours_from_now.count).to eq(1)
+    end
+
+    it 'finds a meeting two days from now' do
+      expect(Meeting.two_days_from_now).to include(meeting_in_two_days)
+      expect(Meeting.two_days_from_now.count).to eq(1)
+    end
+
+  end
+
 end
