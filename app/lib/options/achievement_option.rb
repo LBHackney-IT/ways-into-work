b7f8b31970779ee0d78c965402963a33a9b6e02a
Chris Evans <evanschris@users.noreply.github.com>
2018-02-08 11:33:48 +0000

    update acheivements design into action plan page

--- app/lib/options/achievement_option.rb
+++ app/lib/options/achievement_option.rb
@@ -12,7 +12,7 @@ class AchievementOption < Option
       new('job_application', I18n.t('advisors.achievement.job_application.title.future'), 'paper-plane'),
       new('interview', I18n.t('advisors.achievement.interview.title.future'), 'street-view'),
       new('placement_volunteering', I18n.t('advisors.achievement.placement_volunteering.title.future'), 'handshake-o'),
-      new('training_started', I18n.t('advisors.achievement.training_started.title.future'), 'graduation-cap'),
+      new('training_started', I18n.t('advisors.achievement.training_started.title.future'), 'book'),
       new('course_completed', I18n.t('advisors.achievement.course_completed.title.future'), 'graduation-cap'),
       new('job_start', I18n.t('advisors.achievement.job_start.title.future'), 'id-badge'),
       new('job_sustained', I18n.t('advisors.achievement.job_sustained.title.future'), 'shield'),
