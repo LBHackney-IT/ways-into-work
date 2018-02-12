326ee1ab5e2b51e8abd8c432ff5ba84281b0f81c
Chris Evans <evanschris@users.noreply.github.com>
2018-02-12 14:47:26 +0000

    update overview page

--- app/decorators/client_decorator.rb
+++ app/decorators/client_decorator.rb
@@ -44,20 +44,12 @@ class ClientDecorator < ApplicationDecorator
     standard_wrapper('Date of Birth:', "#{client.date_of_birth.to_date.to_formatted_s(:long)} (#{client.age_in_years} years old)")
   end
 
-  def decorate_objectives
-    standard_wrapper('Objectives:', ObjectiveOption.display(client.objectives))
-  end
-
   def decorate_support_priorities
     standard_wrapper('Support priorities:', SupportOption.display(client.support_priorities))
   end
 
   def decorate_types_of_work
     standard_wrapper('Industry preference:', TypeOfWorkOption.display(client.types_of_work))
-  end
-
-  def decorate_barriers
-    standard_wrapper('Barriers:', BarrierOption.display(client.barriers))
   end
 
   # def decorate_rag_status
@@ -76,13 +68,47 @@ class ClientDecorator < ApplicationDecorator
     standard_wrapper('Currently employed:', value_from(client.employed))
   end
 
-  def decorate_job_goal1
-    standard_wrapper('Job Goal 1:', client.assessment_notes.where(content_key: 'job_goal_1').pluck(:content).join)
+  def decorate_meeting_date(meeting)
+    if meeting.start_datetime && meeting.start_datetime < DateTime.now
+      "Last meeting:"
+    else
+      "Next meeting:"
+    end
   end
 
-  def decorate_job_goal2
-    standard_wrapper('Job Goal 2:', client.assessment_notes.where(content_key: 'job_goal_2').pluck(:content).join)
+  def decorate_meeting_agenda(meeting)
+    string = "Agenda: "
+    string += MeetingAgendaOption.find(meeting.agenda).try(:name) || meeting.other_agenda
   end
+
+  def decorate_task_title
+    if client.action_plan_tasks.ongoing.any?
+      "Next task to complete:"
+    else
+      "Most recently completed task:"
+    end
+  end
+
+  def decorate_single_task_due_date
+    h.content_tag :p do
+      "Due date: " + client.action_plan_tasks.ongoing.first.due_date.to_date.to_s(:short)
+    end
+  end
+
+  def decorate_single_task_title
+    h.content_tag :p, client.action_plan_tasks.ongoing.first.title
+  end
+
+  def decorate_single_task
+    if client.action_plan_tasks.ongoing.any?
+      h.link_to h.edit_advisor_client_action_plan_task_path(client_id: client.id, id: client.action_plan_tasks.ongoing.first.id) do
+        decorate_single_task_title + decorate_single_task_due_date
+      end
+    else
+      h.link_to client.action_plan_tasks.completed.first.title, h.edit_advisor_client_action_plan_task_path(client_id: client.id, id: client.action_plan_tasks.completed.first.id)
+    end
+  end
+
 
   def decorate_preferred_contact
     if client.preferred_contact_method
