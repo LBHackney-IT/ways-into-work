ed1587678c1f8344ae94095c025e77a0611b9cfc
Jason Rae <jrae@wearefuturegov.com>
2018-02-13 10:52:18 -0800

    Simplify decent exposure for new advisor

--- app/controllers/advisor/advisor_controller.rb
+++ app/controllers/advisor/advisor_controller.rb
@@ -1,9 +1,11 @@
 class Advisor::AdvisorController < Advisor::BaseController
   before_action :check_permissions!
-  expose :advisor, build: ->(params, scope) { scope.new(params.merge(login: UserLogin.new)) }
+  expose :advisor
 
-  def new; end
-  
+  def new
+    advisor.build_login
+  end
+
   def create
     advisor.login.generate_default_password
     if advisor.save
@@ -14,15 +16,14 @@ class Advisor::AdvisorController < Advisor::BaseController
       render :new
     end
   end
-  
+
   private
-  
+
   def check_permissions!
     not_authorised unless current_advisor.admin?
   end
-  
+
   def advisor_params
     params.require(:advisor).permit(:name, :phone, :hub_id, :role, login_attributes: [:email])
   end
-  
 end
