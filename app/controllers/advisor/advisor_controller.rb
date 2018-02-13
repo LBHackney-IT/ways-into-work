c16e0a16b29e823ee4dc56e06d3520773c642b08
pezholio <pezholio@gmail.com>
2018-02-13 13:48:55 +0000

    Add creation stuff to controller

--- app/controllers/advisor/advisor_controller.rb
+++ app/controllers/advisor/advisor_controller.rb
@@ -1,15 +1,25 @@
 class Advisor::AdvisorController < Advisor::BaseController
   before_action :check_permissions!
-  expose :advisor
+  expose :advisor, build: ->(params, scope) { scope.new(params.merge(login: UserLogin.new)) }
 
   def new; end
   
-  def create; end
+  def create
+    if advisor.save
+      redirect_to user_root
+    else
+      render :new
+    end
+  end
   
   private
   
   def check_permissions!
     not_authorised unless current_advisor.admin?
+  end
+  
+  def advisor_params
+    params.require(:advisor).permit(:name, :phone, :hub_id, :role, login_attributes: [:email])
   end
   
 end
--- spec/controllers/advisor/advisor_controller_spec.rb
+++ spec/controllers/advisor/advisor_controller_spec.rb
@@ -15,6 +15,41 @@ RSpec.describe Advisor::AdvisorController, type: :controller do
       end
       
     end
+    
+    describe '#create' do
+      
+      let(:params) do
+        {
+          advisor: {
+            name: 'Me',
+            phone: '12345',
+            role: 'advisor',
+            hub_id: Fabricate(:hub).id,
+            login_attributes: {
+              email: 'someone@example.com'
+            }
+          }
+        }
+      end
+      
+      let(:subject) { post :create, params: params }
+      let(:created_advisor) { Advisor.last }
+      
+      it 'creates an advisor' do
+        expect { subject }.to change { Advisor.count }.by(1)
+      end
+      
+      it 'creates the correct things' do
+        subject
+        expect(created_advisor.name).to eq(params[:advisor][:name])
+        expect(created_advisor.phone).to eq(params[:advisor][:phone])
+        expect(created_advisor.role).to eq(params[:advisor][:role])
+        expect(created_advisor.hub_id).to eq(params[:advisor][:hub_id])
+        expect(created_advisor.login.email).to eq(params[:advisor][:login_attributes][:email])
+      end
+      
+      
+    end
   end
   
   context 'as an advisor' do
