68d8cd921d26ef848de4c37f456255f8daeffe1e
Jason Rae <jrae@wearefuturegov.com>
2018-02-13 23:03:24 -0800

    Sort redirect back after restoring client

--- app/controllers/advisor/restore_clients_controller.rb
+++ app/controllers/advisor/restore_clients_controller.rb
@@ -1,9 +1,8 @@
 class Advisor::RestoreClientsController < Advisor::BaseController
-
-  expose :client, -> {Client.with_deleted.find(params[:client_id])}
+  expose :client, -> { Client.with_deleted.find(params[:client_id]) }
 
   def update
     client.restore!
-    redirect_to :advisor_clients
+    redirect_back(fallback_location: :advisor_clients)
   end
 end
