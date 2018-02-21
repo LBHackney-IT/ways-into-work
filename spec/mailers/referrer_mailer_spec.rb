952fa2ba6799d4f355ff17622d259f73149af684
pezholio <pezholio@gmail.com>
2018-02-21 16:48:52 +0000

    Send confirmation email to referrer

---
+++ spec/mailers/referrer_mailer_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+RSpec.describe ReferrerMailer, type: :mailer do
+  
+  let(:client) { Fabricate(:client) }
+  let(:referrer) { Fabricate(:referrer, client: client) }
+
+  describe 'notify_client_signed_up' do
+    
+    let(:mail) { ReferrerMailer.confirmation_email(referrer) }
+  
+    it 'sends to the referrer' do
+      expect(mail.to).to eq([referrer.email])
+    end
+    
+    it 'includes the referrer name' do
+      expect(mail.body.encoded).to include("Hi, #{referrer.name}")
+    end
+    
+    it 'includes the client name' do
+      expect(mail.body.encoded).to include("Thank you for referring #{client.name} on Hackney Works")
+    end
+    
+  end
+  
+end
