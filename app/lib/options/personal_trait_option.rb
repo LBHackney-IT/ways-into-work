88e8810053c1936302069278a232bdf16f73d30a
Chris Evans <evanschris@users.noreply.github.com>
2018-02-21 15:03:00 +0000

    change order to alphabetical for all multi choice options in client profile

--- app/lib/options/personal_trait_option.rb
+++ app/lib/options/personal_trait_option.rb
@@ -1,15 +1,15 @@
 class PersonalTraitOption < Option
   def self.all # rubocop:disable  Metrics/AbcSize, Metrics/MethodLength
     [
-      new('creative', I18n.t('clients.personal_trait.creative')),
       new('active', I18n.t('clients.personal_trait.active')),
       new('caring', I18n.t('clients.personal_trait.caring')),
+      new('creative', I18n.t('clients.personal_trait.creative')),
+      new('good_listener', I18n.t('clients.personal_trait.good_listener')),
       new('organised', I18n.t('clients.personal_trait.organised')),
-      new('problem_solver', I18n.t('clients.personal_trait.problem_solver')),
+      new('outgoing', I18n.t('clients.personal_trait.outgoing')),
       new('people_person', I18n.t('clients.personal_trait.people_person')),
       new('persuasive', I18n.t('clients.personal_trait.persuasive')),
-      new('good_listener', I18n.t('clients.personal_trait.good_listener')),
-      new('outgoing', I18n.t('clients.personal_trait.outgoing'))
+      new('problem_solver', I18n.t('clients.personal_trait.problem_solver'))
     ]
   end
 end
