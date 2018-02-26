6fded3a01291a204ad63cbe30a8c2f93f4f5d498
Chris Evans <evanschris@users.noreply.github.com>
2018-02-26 15:08:32 +0000

    remove extra nav for old root page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def current_header
     if !user_login_signed_in?
-      home? ? 'layouts/site_header_hackney_opportunities' : 'layouts/site_header_base'
+      'layouts/site_header_base'
     elsif current_user_login.user_id.blank?
       'layouts/site_header_client_registration'
     elsif current_user_login.user_type == 'Client'
