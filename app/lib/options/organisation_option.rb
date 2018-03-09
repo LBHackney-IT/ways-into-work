9b4918396759841136a9b342b6a1849081554119
Chris Evans <evanschris@users.noreply.github.com>
2018-03-09 16:06:54 +0000

    add new options to refferals

--- app/lib/options/organisation_option.rb
+++ app/lib/options/organisation_option.rb
@@ -1,15 +1,19 @@
 class OrganisationOption < Option
   def self.all
     [
-      new('benefits', 'Benefits'),
       new('adult_social_care', 'Adult Social Care'),
+      new('benefits', 'Benefits'),
       new('children_and_young_people', 'Children and Young People\'s Services'),
+      new('job_centre_finsbury', 'Job Centre Plus - Finsbury Park'),
+      new('job_centre_mare', 'Job Centre Plus - Mare Street'),
+      new('city_mind', 'Hackney and City Mind'),
       new('adult_and_community_learning', 'HLT Adult and Community Learning'),
+      new('manor_house', 'Manor House Development Trust'),
       new('hackney_housing', 'Tenancy and Leasehold Services (Hackney Housing)'),
       new('other', 'Other')
     ]
   end
-  
+
   def self.display(id)
     find(id)&.name || id
   end
