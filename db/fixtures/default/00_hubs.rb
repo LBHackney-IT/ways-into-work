1d2223058a732ee32ed55c77022b5d64191cf4a1
Chris Evans <evanschris@users.noreply.github.com>
2018-03-26 17:02:11 +0100

    update hubs to creect locations

--- db/fixtures/default/00_hubs.rb
+++ db/fixtures/default/00_hubs.rb
@@ -10,29 +10,29 @@ hubs = [
     ward_mapit_codes: %w[144396 144392 144382 144381 144380 144391 144379]
   }, {
     id: 2,
-    name: 'Redmond Community Centre',
-    address_line_1: 'Kayani Avenue',
+    name: 'Woodberry Down Hub',
+    address_line_1: 'Redmond Community Centre, Kayani Avenue',
     address_line_2: 'Woodberry Down',
     postcode: 'N4 2HF',
-    longitude: -0.0936387,
-    latitude: 51.570132,
+    longitude: -0.0917362,
+    latitude: 51.5701079,
     ward_mapit_codes: %w[144398 144388 144399 144387 144397 144384 144390]
   }, {
     id: 3,
     name: 'Homerton Hub',
-    address_line_1: 'Homerton High Road',
-    postcode: 'E9 6AS',
-    longitude: -0.0438469,
-    latitude: 51.5488747,
+    address_line_1: '6a Rectory Road',
+    postcode: 'N16 7QS',
+    longitude: -0.0700934,
+    latitude: 51.5551779,
     ward_mapit_codes: %w[144385 144386 144389 144395 144394 144383 144393]
   }, {
     id: 4,
-    name: 'Supported Employment'
-    # address_line_1: 'Homerton High Road',
-    # postcode: 'E9 6AS',
-    # longitude: -0.0438469,
-    # latitude: 51.5488747,
-    # ward_mapit_codes: ['144385', '144386', '144389', '144395', '144394', '144383', '144393' ]
+    name: 'Supported Employment Homerton Hub',
+    address_line_1: '6a Rectory Road',
+    postcode: 'N16 7QS',
+    longitude: -0.0700934,
+    latitude: 51.5551779,
+    ward_mapit_codes: %w[144385 144386 144389 144395 144394 144383 144393]
   }
 ]
 
