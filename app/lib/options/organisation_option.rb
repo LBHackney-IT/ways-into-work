18dfe8a056fe7ebd91d7093928a3d80d0ceffa59
pezholio <pezholio@gmail.com>
2018-07-16 11:49:10 +0100

    Add Job Centre Plus - Hoxton as an option

--- app/lib/options/organisation_option.rb
+++ app/lib/options/organisation_option.rb
@@ -6,6 +6,7 @@ class OrganisationOption < Option
       new('children_and_young_people', 'Children and Young People\'s Services'),
       new('job_centre_finsbury', 'Job Centre Plus - Finsbury Park'),
       new('job_centre_mare', 'Job Centre Plus - Mare Street'),
+      new('job_centre_hoxton', 'Job Centre Plus - Hoxton'),
       new('city_mind', 'Hackney and City Mind'),
       new('adult_and_community_learning', 'HLT Adult and Community Learning'),
       new('manor_house', 'Manor House Development Trust'),
