7587bc846dabee3046e98ad6b0e6f09382ed668e
pezholio <pezholio@gmail.com>
2018-07-17 10:25:20 +0100

    Update funded option

--- app/lib/options/funded_option.rb
+++ app/lib/options/funded_option.rb
@@ -1,7 +1,7 @@
 class FundedOption < Option
   def self.all
     [
-      new('troubled_families', 'TF (Troubled Families)'),
+      new('multi_agency_partnership', 'MAP (Multi Agency Partnership)'),
       new('flexible_support_fund', 'FSF (Flexible Support Fund)'),
       new('supported_employment', 'SE (Supported Employment)'),
       new('hackney_works_general', 'HW (Hackney Works - General)'),
