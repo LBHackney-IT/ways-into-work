2580f89e925d4c38e843b950c2e3af3664303861
DomFreeman <dominic.freeman123@gmail.com>
2019-03-19 11:33:32 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181023105626) do
+ActiveRecord::Schema.define(version: 20190319112227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,27 +39,6 @@ ActiveRecord::Schema.define(version: 20181023105626) do
     t.datetime "updated_at", null: false
     t.index ["advisor_id"], name: "index_action_plan_tasks_on_advisor_id"
     t.index ["client_id"], name: "index_action_plan_tasks_on_client_id"
-  end
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "advisors", force: :cascade do |t|
@@ -150,6 +129,27 @@ ActiveRecord::Schema.define(version: 20181023105626) do
     t.index ["client_id"], name: "index_contact_notes_on_client_id"
   end
 
+  create_table "enquiries", force: :cascade do |t|
+    t.integer "client_id"
+    t.integer "opportunity_id"
+    t.string "opportunity_type"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "external_apprenticeships", force: :cascade do |t|
+    t.string "title"
+    t.string "short_description"
+    t.datetime "end_date"
+    t.string "salary"
+    t.text "long_description"
+    t.string "reference_number"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "featured_vacancies", force: :cascade do |t|
     t.integer "position"
     t.bigint "vacancy_id"
@@ -179,6 +179,18 @@ ActiveRecord::Schema.define(version: 20181023105626) do
     t.string "ward_mapit_codes", default: [], array: true
     t.bigint "manager_id"
     t.index ["manager_id"], name: "index_hubs_on_manager_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "short_description"
+    t.datetime "end_date"
+    t.string "salary"
+    t.text "long_description"
+    t.string "reference_number"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "meetings", force: :cascade do |t|
