ff3b6e3fd23a230a801f90e50e12d754ef4e0715
DomFreeman <dominic.freeman123@gmail.com>
2019-03-20 12:16:54 +0000

    Add validations to EAs

--- app/models/external_apprenticeship.rb
+++ app/models/external_apprenticeship.rb
@@ -1,6 +1,12 @@
 class ExternalApprenticeship < ApplicationRecord
   include Opportunity
 
+  validates :title, presence: true
+  validates :end_date, presence: true
+  validates :short_description, presence: true
+  validates :location, presence: true
+  validates :salary, presence: true
+
   has_many :enquiries, as: :opportunity
 
 end
