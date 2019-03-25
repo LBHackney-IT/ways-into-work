c9218691fd15d8e7eb178755ca397752cef8ea64
DomFreeman <dominic.freeman123@gmail.com>
2019-03-25 12:39:36 +0000

    Add very basic enquiry review page

--- app/controllers/advisor/enquiries_controller.rb
+++ app/controllers/advisor/enquiries_controller.rb
@@ -1,6 +1,24 @@
 class Advisor::EnquiriesController < Advisor::BaseController
   expose :client
+  expose :opportunity
+  expose :enquiry
 
   def index; end
+
+  def show
+  end
+
+  def update
+    if enquiry.update_attributes(enquiry_params)
+      flash[:success] = "Enquiry processed"
+      redirect_to opportunity_path(opportunity)
+    else
+      render 'show'
+    end
+  end
+
+  def enquiry_params
+    params.require(:enquiry).permit(:feedback, :status)
+  end
 
 end
