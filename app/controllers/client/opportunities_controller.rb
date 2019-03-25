03874e87df6dcc74281fa34b6f8042f95a8e0b06
DomFreeman <dominic.freeman123@gmail.com>
2019-03-25 11:50:42 +0000

    Make opportunity index and show availble to unauthed users and share some views. Also have single opp show template rather than separate for each type.  delete now unnecessary views and controllers.

--- app/controllers/client/opportunities_controller.rb
+++
@@ -1,7 +0,0 @@
-class Client::OpportunitiesController < Client::BaseController
-  def index
-    @opportunities = Opportunity.active_ending_first
-    render 'shared/opportunities/index'
-  end
-
-end
--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -2,12 +2,10 @@ class OpportunitiesController < ApplicationController
   def index
     @opportunities = Opportunity.active
     @old_opportunities = Opportunity.inactive
-    render 'shared/opportunities/index'
   end
 
   def show
     @opportunity = Opportunity.find(params[:id])
-    render 'shared/opportunities/show'
   end
 
 end
