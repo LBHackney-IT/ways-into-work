ff4548045018dc56320ee6a029ab909a69fd3d04
Chris Evans <evanschris@users.noreply.github.com>
2019-03-26 15:20:16 +0000

    set status to be required

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -2,6 +2,8 @@ class Enquiry < ApplicationRecord
   belongs_to :client
   belongs_to :opportunity
 
+  validates :status, presence: true
+
   enum status: %i[awaiting accepted declined]
 
   scope :awaiting, -> { where('status = 0').order(:created_at) }
