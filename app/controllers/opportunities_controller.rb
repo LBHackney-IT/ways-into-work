e32e31d334ef2500fe95e76bdbcccdec36871bb3
DomFreeman <dominic.freeman123@gmail.com>
2019-04-02 14:32:51 +0100

    Do not let clients or public view expired opportunities

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -6,6 +6,10 @@ class OpportunitiesController < ApplicationController
 
   def show
     @opportunity = Opportunity.find(params[:id])
+    if !current_advisor && @opportunity.closing_date < Date.today
+      flash[:notice] = "Opportunity has expired"
+      redirect_to opportunities_path
+    end
   end
 
 end
