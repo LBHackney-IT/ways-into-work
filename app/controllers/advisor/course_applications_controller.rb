35f5078140f822d7be7b563ba1fc2f9facaba726
userman123 <dominic.freeman123@gmail.com>
2020-07-27 14:33:37 +0100

    Start to integrate wp API for applications

--- app/controllers/advisor/course_applications_controller.rb
+++ app/controllers/advisor/course_applications_controller.rb
@@ -2,12 +2,15 @@ class Advisor::CourseApplicationsController < Advisor::BaseController
 
   def index
     @course_applications = CourseApplication.all
+    response = HTTParty.get("https://hackney-works-staging.hackney.gov.uk/wp-json/wp/v2/intake")
+    @intakes = response.parsed_response
   end
 
   def show
     @course_application = CourseApplication.find(params[:id])
     response = HTTParty.get("https://hackney-works-staging.hackney.gov.uk/wp-json/wp/v2/intake")
     @intake = response.parsed_response[0]
+    @course = @intake["acf"]["parent_course"]
   end
 
 end
--- app/models/course_application.rb
+++ app/models/course_application.rb
@@ -1,3 +1,5 @@
 class CourseApplication < ApplicationRecord
-
+  def total_applications
+    CourseApplication.where(intake_id: self.intake_id).count
+  end
 end
