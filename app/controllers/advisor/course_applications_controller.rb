b56c30f849a4ef5b8df289793f5d88f461f5bd88
userman123 <dominic.freeman123@gmail.com>
2020-07-27 14:41:43 +0100

    Allow acceepting or rejecting of application:

--- app/controllers/advisor/course_applications_controller.rb
+++ app/controllers/advisor/course_applications_controller.rb
@@ -13,4 +13,21 @@ class Advisor::CourseApplicationsController < Advisor::BaseController
     @course = @intake["acf"]["parent_course"]
   end
 
+  def update
+    @course_application = CourseApplication.find(params[:id])
+    response = HTTParty.get("https://hackney-works-staging.hackney.gov.uk/wp-json/wp/v2/intake")
+    @intake = response.parsed_response[0]
+    @course = @intake["acf"]["parent_course"]
+    if @course_application.update_attributes(course_application_params)
+      flash[:success] = "Enquiry review updated"
+      #redirect_to session[:return_to]
+      render 'show'
+    else
+      render 'show'
+    end
+  end
+
+  def course_application_params
+    params.require(:course_application).permit(:first_name, :last_name, :phone_number, :email, :intake_id, :status)
+  end
 end
--- app/models/course_application.rb
+++ app/models/course_application.rb
@@ -2,4 +2,8 @@ class CourseApplication < ApplicationRecord
   def total_applications
     CourseApplication.where(intake_id: self.intake_id).count
   end
+
+  def accepted_applications
+    CourseApplication.where(intake_id: self.intake_id, status: 'accepted').count
+  end
 end
