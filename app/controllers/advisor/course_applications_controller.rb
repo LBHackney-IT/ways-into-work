cc275ce2d9c658ca4b16961fa541b9cc9cf40f67
userman123 <dominic.freeman123@gmail.com>
2020-10-27 11:59:13 +0000

    Bugfix - intake not found on course application show

--- app/controllers/advisor/course_applications_controller.rb
+++ app/controllers/advisor/course_applications_controller.rb
@@ -44,8 +44,8 @@ class Advisor::CourseApplicationsController < Advisor::BaseController
 
   def get_intake_and_course
     response = HTTParty.get("#{ENV['WORDPRESS_DOMAIN']}/wp-json/wp/v2/intake/#{@course_application.intake_id}")
-    @intake = response.parsed_response
-    @course = @intake["acf"]["parent_course"]
+    @intake = response.parsed_response if response.parsed_response["id"]
+    @course = @intake["acf"]["parent_course"] if @intake
   end
 
 end
