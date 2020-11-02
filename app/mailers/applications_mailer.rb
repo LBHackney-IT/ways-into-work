d9458c53f90818cac7304ad57158d421db5131c0
userman123 <dominic.freeman123@gmail.com>
2020-11-02 13:54:00 +0000

    Bugfix and refactor emails

--- app/mailers/applications_mailer.rb
+++ app/mailers/applications_mailer.rb
@@ -3,9 +3,9 @@ class ApplicationsMailer < ApplicationMailer
   def confirm_application(application)
     @application = application
     if @application.type == 'CourseApplication'
-      @title = HTTParty.get("#{ENV['WORDPRESS_DOMAIN']}/wp-json/wp/v2/intake/#{@application.wordpress_object_id}").parsed_response["acf"]["parent_course"]["post_title"]
+      set_intake_title(application)
     elsif application.type == 'VacancyApplication'
-      @title = HTTParty.get("#{ENV['WORDPRESS_DOMAIN']}/wp-json/wp/v2/vacancy/#{@application.wordpress_object_id}").parsed_response["title"]["rendered"]
+      set_vacancy_title(@application)
     end
 
     mail(
@@ -19,15 +19,40 @@ class ApplicationsMailer < ApplicationMailer
 
     if @application.type == 'CourseApplication'
       to_email = WaysIntoWork.config.learning_team_email
-      @title = HTTParty.get("#{ENV['WORDPRESS_DOMAIN']}/wp-json/wp/v2/intake/#{@application.wordpress_object_id}").parsed_response["acf"]["parent_course"]["post_title"]
+
+      set_intake_title(@application)
+
     elsif application.type == 'VacancyApplication'
       to_email = WaysIntoWork.config.vacancy_team_email
-      @title = HTTParty.get("#{ENV['WORDPRESS_DOMAIN']}/wp-json/wp/v2/vacancy/#{@application.wordpress_object_id}").parsed_response["title"]["rendered"]
+
+      set_vacancy_title(@application)
+
     end
 
     mail(
       to: to_email,
       subject: I18n.t('course_applications.mail.subject.notify_team_of_new_application')
     )
+  end
+
+  def set_intake_title application
+    response = HTTParty.get("#{ENV['WORDPRESS_DOMAIN']}/wp-json/wp/v2/intake/#{application.wordpress_object_id}")
+
+    if response.parsed_response["id"]
+      @intake = response.parsed_response
+      @title = @intake["acf"]["parent_course"]["post_title"]
+    else
+      @title = 'intake not found'
+    end
+  end
+
+  def set_vacancy_title application
+    response = HTTParty.get("#{ENV['WORDPRESS_DOMAIN']}/wp-json/wp/v2/vacancy/#{application.wordpress_object_id}")
+    if response.parsed_response["id"]
+      @vacancy = response.parsed_response
+      @title = @vacancy["title"]["rendered"]
+    else
+      @title = 'vacancy not found'
+    end
   end
 end
