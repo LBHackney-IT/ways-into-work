cd8f1a2bcb5b8e3328b9dcc2c6c5c70d49f9408f
userman123 <dominic.freeman123@gmail.com>
2020-11-03 17:13:10 +0000

    Put refer nav back in and temporarily allow advisors to view opps for scotty

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,6 +1,11 @@
 class OpportunitiesController < ApplicationController
   def index
     redirect_to ENV['WORDPRESS_DOMAIN'] unless current_advisor
+    filter_type = params[:type] || 'all'
+    if ["all", "jobs", "apprenticeships", "placements", "events", "training"].include?(filter_type)
+      @opportunities = Opportunity.send(filter_type).active.page params[:page]
+      @old_opportunities = Opportunity.send(filter_type).inactive.page params[:page]
+    end
   end
 
   def show
