24fdd636ddc60853d0d33c1420bba9fcc460a559
georgeschena <georgeschena@hotmail.com>
2024-10-04 09:47:29 +0100

    Add redirect for the support page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,23 +1,25 @@
 # :nocov:
 class StaticController < ApplicationController
-  
+
   def apprenticeships; end
-  
+
   def hackney100; end
-  
-  def support; end
-  
+
+  def support
+    redirect_to 'http://opportunities.hackney.gov.uk/'
+  end
+
   def employers; end
-  
+
   def privacy_policy; end
-  
+
   def outside_hackney; end
-  
+
   def referrer_outside_hackney; end
-  
+
   def just_registered; end
-  
+
   def unsubscribed; end
-  
+
 end
 # :nocov:
